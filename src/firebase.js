9ddb2b4b81d60f8c80746b6811cf42ea38adabe4
teeoomaan <teomandagustu@gmail.com>
2026-06-27 03:07:23 +0300

    online denemesi

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+// İleride veritabanı (Firestore) ve giriş (Auth) işlemlerini buradan yöneteceğiz.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDqR_PnpJDyMwAq_JTR28jNCYMW3KRMuh8",
+  authDomain: "mapletics-e1e53.firebaseapp.com",
+  projectId: "mapletics-e1e53",
+  storageBucket: "mapletics-e1e53.firebasestorage.app",
+  messagingSenderId: "830981688983",
+  appId: "1:830981688983:web:13e5136321aa01e81e23d2",
+  measurementId: "G-LWVLJ2ZCP6"
+};
+
+// Firebase'i Başlat
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
